ac2817ff5086320e81c59fe70f4bad3e852942b8
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-01-28 20:03:54 +0530

    Edited relop

--- App02.java
+++ App02.java
@@ -32,7 +32,7 @@ public class App02 {
         double max = 0;
         int index = 0;
         for (int i = 0; i < num; i++) {
-            if (employees[i].dept.compareTo("purchase") == 0 & employees[i].des.compareTo("manager") == 0) {
+            if (employees[i].dept.compareTo("purchase") == 0 && employees[i].des.compareTo("manager") == 0) {
                 if (employees[i].sal > max) {
                     max = employees[i].sal;
                     index = i;
