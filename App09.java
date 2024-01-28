50bc3aa398b5c74a140b169e8486695759257bed
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-01-29 02:34:32 +0530

    Gb to Bytes

--- App09.java
+++ App09.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class App09 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Compute GBConverter = new KBGBConverter();
-        System.out.print("Enter number of Kilo Bytes (KBs): ");
-        double kb = scan.nextDouble();
-        double gb = GBConverter.convert(kb);
-        System.out.println(kb + " KB is equal to " + gb + " GB");
+
+        Compute GBConverter = new BGBConverter();
+        System.out.print("Enter number of Giga Bytes (GBs): ");
+        double gb = scan.nextDouble();
+        double b = GBConverter.convert(gb);
+        System.out.println(gb + " GB is equal to " + b + "B");
+
         Compute EuroConverter = new EuroRsConverter();
         System.out.print("Enter amount in Euros: ");
         double eur = scan.nextDouble();
@@ -23,10 +25,10 @@ interface Compute {
     double convert(double value);
 }
 
-class KBGBConverter implements Compute {
-    public double convert(double kb) {
-        // 1 GB = 1000*1000 KB
-        return (kb / (1000 * 1000));
+class BGBConverter implements Compute {
+    public double convert(double gb) {
+        // 1 GB = 1000*1000*1000 B
+        return (gb * 1000 * 1000 * 1000);
     }
 }
 
@@ -38,7 +40,10 @@ class EuroRsConverter implements Compute {
 }
 
 // Output:
-// Enter number of Kilo Bytes (KBs): 396478000
-// 3.96478E8 KB is equal to 396.478 GB
+// Enter number of Giga Bytes (GBs): 2.43
+// 2.43 GB is equal to 2.43E9B
 // Enter amount in Euros: 420
 // 420.0 Euros is equal to 37800.0 Rupees
+
+// Enter number of Kilo Bytes (KBs): 396478000
+// 3.96478E8 KB is equal to 396.478 GB
